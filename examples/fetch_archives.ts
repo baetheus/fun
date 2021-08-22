33b024820c61cf281dc9f8df0da0264dfa5d82e9
David Lonyai <david.lonyai@gmail.com>
2021-08-22 19:00:01 +0200

    fix: update to latest Schemable

--- examples/fetch_archives.ts
+++ examples/fetch_archives.ts
@@ -1,6 +1,7 @@
 import * as A from "../array.ts";
 import * as D from "../schemable/decoder.ts";
 import * as E from "../either.ts";
+import * as S from "../schemable/schemable.ts";
 import * as TE from "../task_either.ts";
 import * as L from "../optics/lens.ts";
 import * as T from "../optics/traversal.ts";
@@ -21,10 +22,8 @@ const decodeError = (error: string): DecodeError => ({
 
 type MyErrors = FetchError | DecodeError;
 
-const foldErrors = <O>(
-  onFetchError: (e: unknown) => O,
-  onDecodeError: (e: string) => O,
-) =>
+const foldErrors =
+  <O>(onFetchError: (e: unknown) => O, onDecodeError: (e: string) => O) =>
   (e: MyErrors) => {
     switch (e.type) {
       case "FetchError":
@@ -37,29 +36,29 @@ const foldErrors = <O>(
 /**
  * Let's make a helper function for fetch. This one takes the same
  * inputs as fetch, but does the following:
- * 
+ *
  * * Wraps fetch in a TaskEither instance
  * * Parses the response to json
  * * Takes any errors and types them as FetchError
  */
 const fetchTaskEither = (
-  url: string | Request | URL,
+  url: string | Request | URL
 ): TE.TaskEither<FetchError, unknown> =>
   // Pipe is a helper function. It takes the first value and passes it to the function in the
   // second position, then takes the result of that and passes it on to a third argument (if
   // it exists). Once it reaches the end, it returns the result.
   pipe(
     () => fetch(url).then((res) => res.json()), // Here is normal fetch and then parsing json
-    TE.fromFailableTask(flow(String, fetchError)), // Here we wrap it in a TaskEither and parse errors
+    TE.fromFailableTask(flow(String, fetchError)) // Here we wrap it in a TaskEither and parse errors
   );
 
 /**
  * Next, let's combine the fetch helper function with a "Decoder"
- * 
+ *
  * A Decoder is simply a function that takes some input and makes
  * sure it has the "shape" or "properties" that we want. It's output
  * is either a tree of errors or the data you want wrapped in an Either.
- * 
+ *
  * Here, we take a decoder, then we return an extension of the
  * fetchTaskEither function where we make sure the response from
  * fetchTaskEither has the structure that we want.
@@ -69,12 +68,15 @@ const fromDecode = <A>(decoder: D.Decoder<A>) =>
   // first argument being a value, it is a function.
   flow(
     fetchTaskEither, // Start with fetchTaskEither
-    TE.chain(flow( // Then take any "good" results and pass them to..
-      decoder, // The decoder
-      E.mapLeft((e) => decodeError(D.draw(e))), // Take any decoder errors and wrap them up
-      TE.fromEither, // Since a decoder returns a plain "Either" we wrap it in a Task to make a TaskEither
-      TE.widen<FetchError>(), // This is necessary so we can have different error types in the flow
-    )),
+    TE.chain(
+      flow(
+        // Then take any "good" results and pass them to..
+        decoder, // The decoder
+        E.mapLeft((e) => decodeError(D.draw(e))), // Take any decoder errors and wrap them up
+        TE.fromEither, // Since a decoder returns a plain "Either" we wrap it in a Task to make a TaskEither
+        TE.widen<FetchError>() // This is necessary so we can have different error types in the flow
+      )
+    )
   );
 
 /**
@@ -82,35 +84,30 @@ const fromDecode = <A>(decoder: D.Decoder<A>) =>
  * for "merging" two decoders together. In this case type contains the required
  * properties on a Document and partial contains the optional ones.
  */
-const Document = pipe(
-  D.struct({
-    title: D.string(),
-    collection: D.array(D.string()), // It's decoders all the way down.
-    downloads: D.number(),
-    format: D.array(D.string()),
-    identifier: D.string(),
-    item_size: D.number(),
-    mediatype: D.string(),
-  }),
-  D.intersect(D.partial({
-    backup_location: D.string(),
-    language: D.string(),
-    date: D.string(),
-    description: pipe(
-      D.string(),
-      D.union(D.array(D.string())),
-    ),
-    creator: pipe(
-      D.string(),
-      D.union(D.array(D.string())),
-    ),
-    month: D.number(),
-    week: D.number(),
-    year: pipe(
-      D.string(),
-      D.union(D.number()),
-    ),
-  })),
+const Document = S.make((d) =>
+  pipe(
+    d.struct({
+      title: d.string(),
+      collection: d.array(d.string()), // It's decoders all the way down.
+      downloads: d.number(),
+      format: d.array(d.string()),
+      identifier: d.string(),
+      item_size: d.number(),
+      mediatype: d.string(),
+    }),
+    d.intersect(
+      d.partial({
+        backup_location: d.string(),
+        language: d.string(),
+        date: d.string(),
+        description: pipe(d.string(), d.union(d.array(d.string()))),
+        creator: pipe(d.string(), d.union(d.array(d.string()))),
+        month: d.number(),
+        week: d.number(),
+        year: pipe(d.string(), d.union(d.number())),
+      })
+    )
+  )
 );
 // TypeOf extracts the type from the decoder so
 // we don't have to do double the work keeping decoders
@@ -119,29 +116,33 @@ type Document = D.TypeOf<typeof Document>;
 
 // A response from archive contains many documents and
 // some metadata
-const Response = D.struct({
-  numFound: D.number(),
-  start: D.number(),
-  docs: D.array(Document),
-});
-type Response = D.TypeOf<typeof Response>;
+const Response = S.make((s) =>
+  s.struct({
+    numFound: s.number(),
+    start: s.number(),
+    docs: s.array(Document(s)),
+  })
+);
+type Response = S.TypeOf<typeof Response>;
 
 // The happy path response from archive gives this response
-const QueryResponse = D.struct({
-  response: Response, // Notice that Decoders are composable even when you make your own
-});
-type QueryResponse = D.TypeOf<typeof QueryResponse>;
+const QueryResponse = S.make((s) =>
+  s.struct({
+    response: Response(s), // Notice that Decoders are composable even when you make your own
+  })
+);
+type QueryResponse = S.TypeOf<typeof QueryResponse>;
 
 // Wireup the fromDecode with QueryResponse
-const fetchDecodeArchive = fromDecode(QueryResponse);
+const fetchDecodeArchive = fromDecode(QueryResponse(D.Schemable));
 
 // This is a simple helper function that lets us only worry
 // about supplying a term to lookup in the internet archive
 const queryArchive = (term: string) =>
   fetchDecodeArchive(
-    `https://archive.org/advancedsearch.php?q=${
-      encodeURIComponent(term)
-    }&output=json`,
+    `https://archive.org/advancedsearch.php?q=${encodeURIComponent(
+      term
+    )}&output=json`
   );
 
 // If we just wanted to print the response we could do so like this
@@ -157,20 +158,20 @@ const getData = pipe(
   L.prop("docs"), // Then on the docs field
   L.traverse(A.Traversable),
   T.props("title", "date", "description"), // We only want to focus on title, date, and description
-  T.getAll, // We've focused on the data we want, let's get it!
+  T.getAll // We've focused on the data we want, let's get it!
 );
 
 // The last part is to run the query and map the response to the getData function
 pipe(
   queryArchive("Chimamanda"), // Fetch and Parse a request for items with Chimamanda in them
-  TE.map(getData), // Take any "good" response and extract an array of titles, dates, and descriptions
+  TE.map(getData) // Take any "good" response and extract an array of titles, dates, and descriptions
   // This line actually runs our program and outputs the results
   // to either stderr or stdout depending on the result
 )().then(
   E.fold(
     foldErrors((e) => console.error("FetchError", e), console.error),
-    console.log,
-  ),
+    console.log
+  )
 );
 
 /**
