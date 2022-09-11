2225869983d8122faf1da5be7170f39e667170d9
brandon@null.pub <brandon@null.pub>
2022-09-11 16:45:20 +0000

    feat: update alt.ts for fun 2.0
    Fixes #64
    
    FossilOrigin-Name: a0303f48bd3e287a82f76cf2cce7f2cbbca1678ac967f1886504b1b92157c552

--- alt.ts
+++ alt.ts
@@ -1,14 +1,36 @@
+/**
+ * Alt presents a type level pattern to select over
+ * multiple algebraic data types.
+ *
+ * The basic concept is something along the lines of,
+ * "If this one doesn't work, try this other one".
+ */
+
 // deno-lint-ignore-file no-explicit-any
 import type { Functor } from "./functor.ts";
 import type { Kind, URIS } from "./kind.ts";
 
 /**
- * Alt
- * https://github.com/fantasyland/static-land/blob/master/docs/spec.md#alt
+ * An instance of Alt extends Functor and provides a new method
+ * called alt. This method takes two matching Kinds, left and right,
+ * and returns one of them. A good way to think of this is as
+ * a boolean or, where if the left kind is "false" then the right
+ * kind is returned.
+ *
+ * An instance of alt must obay the following laws:
+ *
+ * 1. Associativity:
+ *    pipe(a, alt(b), alt(c)) === pipe(a, alt(pipe(b, alt(c))))
+ * 2. Distributivity:
+ *    pipe(a, alt(b), map(f)) === pipe(a, map(f), alt(pipe(b, map(f))))
+ *
+ * The original type came from
+ * [here](https://github.com/fantasyland/static-land/blob/master/docs/spec.md#alt)
  */
-export interface Alt<URI extends URIS, _ extends any[] = any[]>
-  extends Functor<URI, _> {
-  readonly alt: <A, B extends _[0], C extends _[1], D extends _[2]>(
-    tb: Kind<URI, [A, B, C, D]>,
-  ) => (ta: Kind<URI, [A, B, C, D]>) => Kind<URI, [A, B, C, D]>;
-}
+export type Alt<URI extends URIS, _ extends any[] = any[]> =
+  & Functor<URI, _>
+  & {
+    readonly alt: <A, B extends _[0], C extends _[1], D extends _[2]>(
+      right: Kind<URI, [A, B, C, D]>,
+    ) => (left: Kind<URI, [A, B, C, D]>) => Kind<URI, [A, B, C, D]>;
+  };
